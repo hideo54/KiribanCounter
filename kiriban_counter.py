6217f56084a997866d55589d83fcef8bcbd1cf0f
hideo54 <hideo54.pub@gmail.com>
2014-06-30 16:55:01 +0900

    commit
    ディレクトリを変えました。

---
+++ kiriban_counter.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+import tweepy
+import datetime
+
+consumer_key = '************'
+consumer_secret = '************'
+access_token = '************'
+access_token_secret = '************'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tweets = api.me().statuses_count
+ketasu = len(str(tweets))
+
+if (tweets + 1) % 100 == 0:
+    api.update_status('just %d tweets!' % (tweets + 1))
+
+elif ketasu != 1:
+    zoro = 0
+    for i in range(ketasu+1)
+        zoro += 10 ** i
+
+    if (tweets + 1) % zoro == 0 and (tweets + 1) / zoro != 9:
+        api.update_status('just %d tweets!' % (tweets + 1))
